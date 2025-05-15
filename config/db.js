643501b6c8a7fa3c88a9e1d6947d2cc1866d5f89
Sebastian <jchingapalacios05@gmail.com>
2025-05-15 14:00:14 -0500

    Modificando db.js

--- config/db.js
+++ config/db.js
@@ -4,7 +4,8 @@ dotenv.config({path: './.env'});
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     dialect: "mysql",
-    host: 'localhost'
+    host: "127.0.0.1",
+    port: '3306'
 })
 
 export default db;
