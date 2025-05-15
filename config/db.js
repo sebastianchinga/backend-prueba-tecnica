e345a5da59003a8de5fb04958b0768c87a4b7a51
Sebastian <jchingapalacios05@gmail.com>
2025-05-15 14:36:07 -0500

    Guardando credenciales

--- config/db.js
+++ config/db.js
@@ -3,8 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config({ path: './.env' });
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: '127.0.0.1',
-    port: '3306',
+    host: 'shuttle.proxy.rlwy.net',
+    port: '32546',
     dialect: 'mysql',
     define: {
         timestamps: false
